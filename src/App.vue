4ddb800c8f4dd2081be0e25891c7267789630216
wepd <weped_x@163.com>
2021-03-07 10:59:54 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+  <my-addItion></my-addItion>
+  <h3>-------------------------</h3>
+  <my-subtraction></my-subtraction>
+</div>
+</template>
+
+<script>
+import addItion from './components/addItion'
+import subtraction from './components/subtraction'
+
+export default {
+  data(){
+    return {
+
+    }
+  },
+  components: {
+    'my-addItion': addItion,
+    'my-subtraction': subtraction 
+  }
+}
+</script>
---
+++ src/components/subtraction.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h3>
+            当前最新的count值为： {{ count}}
+        </h3>
+        <button>-</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {}
+    }
+}
+</script>
